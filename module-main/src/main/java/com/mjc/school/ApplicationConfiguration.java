45b493cd13b45b9abf2dab16a0f9d2cb06af12cf
Edgar Vardanyan <e.vardan4an@gmail.com>
2023-03-06 02:00:47 +0400

    upgraded to springboot and jpa

--- module-main/src/main/java/com/mjc/school/ApplicationConfiguration.java
+++ module-main/src/main/java/com/mjc/school/ApplicationConfiguration.java
@@ -1,11 +1,46 @@
-package com.mjc.school;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-
-@Configuration
-@ComponentScan(basePackages = "com.mjc.school")
-@EnableAspectJAutoProxy
-public class ApplicationConfiguration {
-}
+//package com.mjc.school;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.EnableAspectJAutoProxy;
+//import org.springframework.data.auditing.DateTimeProvider;
+//import org.springframework.jdbc.datasource.DriverManagerDataSource;
+//
+//import javax.sql.DataSource;
+//import java.time.OffsetDateTime;
+//import java.time.ZoneOffset;
+//import java.util.Optional;
+//import java.util.Properties;
+//
+//@Configuration
+//@ComponentScan(basePackages = "com.mjc.school")
+//@EnableAspectJAutoProxy
+//public class ApplicationConfiguration {
+//    @Bean(name = "iso8601DateTimeProvider")
+//    public DateTimeProvider dateTimeProvider() {
+//        return () -> Optional.of(OffsetDateTime.now(ZoneOffset.UTC));
+//    }
+//
+//    @Bean
+//    public DataSource dataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("org.postgresql.Driver");
+//        dataSource.setUrl("jdbc:postgresql://localhost:5432/epam_news");
+//        dataSource.setUsername("epam_news");
+//        dataSource.setPassword("password");
+//
+//        return dataSource;
+//    }
+//
+//    @Bean
+//    public Properties hibernateProperties() {
+//        Properties properties = new Properties();
+//        properties.setProperty("hibernate.connection.pool_size", "3");
+//        properties.setProperty("hibernate.hbm2ddl.auto", "update");
+//        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+//        properties.setProperty("hibernate.show_sql", "true");
+//
+//        return properties;
+//    }
+//}
--- module-main/src/main/java/com/mjc/school/HibernateConfiguration.java
+++ module-main/src/main/java/com/mjc/school/HibernateConfiguration.java
@@ -1,59 +1,59 @@
-package com.mjc.school;
-
-import org.hibernate.SessionFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
-import java.util.Properties;
-
-
-@Configuration
-@EnableTransactionManagement
-public class HibernateConfiguration {
-
-    @Bean
-    public LocalSessionFactoryBean sessionFactory() {
-        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
-        sessionFactory.setDataSource(dataSource());
-        sessionFactory.setPackagesToScan("com.mjc.school.repository");
-//        sessionFactory.setAnnotatedClasses(User.class);
-        sessionFactory.setHibernateProperties(hibernateProperties());
-
-        return sessionFactory;
-    }
-
-    @Bean
-    public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/epam_news");
-        dataSource.setUsername("epam_news");
-        dataSource.setPassword("password");
-
-        return dataSource;
-    }
-
-
-    private Properties hibernateProperties() {
-        Properties properties = new Properties();
-        properties.setProperty("hibernate.connection.pool_size", "3");
-//        properties.setProperty("hibernate.current_session_context_class", "thread");
-        properties.setProperty("hibernate.hbm2ddl.auto", "update");
-        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
-        properties.setProperty("hibernate.show_sql", "true");
-
-        return properties;
-    }
-
-    @Bean
-    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
-        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
-        transactionManager.setSessionFactory(sessionFactory);
-        return transactionManager;
-    }
-}
+//package com.mjc.school;
+//
+//import org.hibernate.SessionFactory;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.jdbc.datasource.DriverManagerDataSource;
+//import org.springframework.orm.hibernate5.HibernateTransactionManager;
+//import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+//import org.springframework.transaction.annotation.EnableTransactionManagement;
+//
+//import javax.sql.DataSource;
+//import java.util.Properties;
+//
+//
+//@Configuration
+//@EnableTransactionManagement
+//public class HibernateConfiguration {
+//
+//    @Bean
+//    public LocalSessionFactoryBean sessionFactory() {
+//        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+//        sessionFactory.setDataSource(dataSource());
+//        sessionFactory.setPackagesToScan("com.mjc.school.repository");
+////        sessionFactory.setAnnotatedClasses(User.class);
+//        sessionFactory.setHibernateProperties(hibernateProperties());
+//
+//        return sessionFactory;
+//    }
+//
+//    @Bean
+//    public DataSource dataSource() {
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("org.postgresql.Driver");
+//        dataSource.setUrl("jdbc:postgresql://localhost:5432/epam_news");
+//        dataSource.setUsername("epam_news");
+//        dataSource.setPassword("password");
+//
+//        return dataSource;
+//    }
+//
+//
+//    private Properties hibernateProperties() {
+//        Properties properties = new Properties();
+//        properties.setProperty("hibernate.connection.pool_size", "3");
+////        properties.setProperty("hibernate.current_session_context_class", "thread");
+//        properties.setProperty("hibernate.hbm2ddl.auto", "update");
+//        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+//        properties.setProperty("hibernate.show_sql", "true");
+//
+//        return properties;
+//    }
+//
+//    @Bean
+//    public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+//        HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+//        transactionManager.setSessionFactory(sessionFactory);
+//        return transactionManager;
+//    }
+//}
