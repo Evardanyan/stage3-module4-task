95b30377d56920554bed27e772d8058dfb37d0ea
Edgar Vardanyan <e.vardan4an@gmail.com>
2023-03-07 23:40:25 +0400

    updated news controller

--- module-web/src/main/java/com/mjc/school/controller/impl/NewsController.java
+++ module-web/src/main/java/com/mjc/school/controller/impl/NewsController.java
@@ -5,12 +5,16 @@ import com.mjc.school.controller.annotation.CommandHandler;
 import com.mjc.school.service.BaseService;
 import com.mjc.school.service.dto.NewsDtoRequest;
 import com.mjc.school.service.dto.NewsDtoResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+@RestController
+@RequestMapping(value = "/api/v1/news", consumes = {"application/JSON"}, produces = {"application/JSON", "application/XML"})
 public class NewsController implements BaseController<NewsDtoRequest, NewsDtoResponse, Long> {
 
     private final BaseService<NewsDtoRequest, NewsDtoResponse, Long> service;
@@ -21,46 +25,47 @@ public class NewsController implements BaseController<NewsDtoRequest, NewsDtoRes
 
 
     @Override
-    @CommandHandler(operation = "1")
-    public List<NewsDtoResponse> readAll() {
-        return service.readAll();
+    public ResponseEntity<List<NewsDtoResponse>> readAll() {
+        List<NewsDtoResponse> news = service.readAll();
+        return ResponseEntity.ok(news);
     }
 
     @Override
-    @CommandHandler(operation = "2")
-    public NewsDtoResponse readById(Long id) {
-        return service.readById(id);
+    @GetMapping(value = "/{id:\\d+}")
+    public ResponseEntity<NewsDtoResponse> readById(@PathVariable Long id) {
+        return ResponseEntity.ok(service.readById(id));
     }
 
     @Override
-    @CommandHandler(operation = "3")
-    public NewsDtoResponse create(NewsDtoRequest createRequest) {
-        return service.create(createRequest);
+    @PostMapping
+    public ResponseEntity<NewsDtoResponse> create(@RequestBody NewsDtoRequest createRequest) {
+        NewsDtoResponse newsDtoResponse = service.create(createRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newsDtoResponse);
     }
 
     @Override
-    @CommandHandler(operation = "4")
-    public NewsDtoResponse update(NewsDtoRequest updateRequest) {
-        return service.update(updateRequest);
+    @PutMapping
+    public ResponseEntity<NewsDtoResponse> update(@RequestBody NewsDtoRequest updateRequest) {
+        NewsDtoResponse newsDtoResponse = service.update(updateRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(newsDtoResponse);
     }
 
     @Override
-    @CommandHandler(operation = "5")
-    public boolean deleteById(Long id) {
-        return service.deleteById(id);
+    @DeleteMapping(value = "/{id:\\d+}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        service.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
-    public NewsDtoResponse getNewsByParams(String tagName, Long tagId, String authorName, String title, String content) {
-        return service.getNewsByParams(tagName, tagId, authorName, title, content);
+    @GetMapping("/search")
+    public ResponseEntity<NewsDtoResponse> getNewsByParams(@RequestParam(required = false) String tagName,
+                                                           @RequestParam(required = false) Long tagId,
+                                                           @RequestParam(required = false) String authorName,
+                                                           @RequestParam(required = false) String title,
+                                                           @RequestParam(required = false) String content) {
+        NewsDtoResponse response = service.getNewsByParams(tagName, tagId, authorName, title, content);
+        HttpStatus status = response != null ? HttpStatus.OK : HttpStatus.NOT_FOUND;
+        return ResponseEntity.status(status).body(response);
     }
 
-    @Override
-    public List<NewsDtoResponse> readTagsByNewsId(Long id) {
-        return new ArrayList<>();
-    }
-
-    @Override
-    public NewsDtoResponse readAuthorByNewsId(Long id) {
-        return null;
-    }
 }
