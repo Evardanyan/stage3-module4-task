cceb97bd96a5a2aa247994d0bb8db6fed5c4fafe
Edgar Vardanyan <e.vardan4an@gmail.com>
2023-04-04 01:56:50 +0400

    added api documentation to the newscontroller

--- module-web/src/main/java/com/mjc/school/controller/impl/NewsController.java
+++ module-web/src/main/java/com/mjc/school/controller/impl/NewsController.java
@@ -2,12 +2,11 @@ package com.mjc.school.controller.impl;
 
 import com.mjc.school.controller.BaseController;
 import com.mjc.school.service.BaseService;
-import com.mjc.school.service.dto.CommentDtoRequest;
 import com.mjc.school.service.dto.NewsDtoRequest;
 import com.mjc.school.service.dto.NewsDtoResponse;
+import io.swagger.annotations.*;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -18,6 +17,7 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping(value = "/api/v1/news")
 @Validated
+@Api(tags = "News Management", produces = "application/json", value = "Operations for creating, updating, retrieving and deleting news in the application")
 public class NewsController implements BaseController<NewsDtoRequest, NewsDtoResponse, Long> {
 
     private final BaseService<NewsDtoRequest, NewsDtoResponse, Long> service;
@@ -29,18 +29,23 @@ public class NewsController implements BaseController<NewsDtoRequest, NewsDtoRes
 
     @Override
     @GetMapping
+    @ApiOperation(value = "Get a list of all news", response = NewsDtoResponse.class, responseContainer = "Page")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved the list of news"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
     public ResponseEntity<Page<NewsDtoResponse>> readAll(
-            @RequestParam(value = "page", defaultValue = "0") int page,
-            @RequestParam(value = "size", defaultValue = "10") int size,
-            @RequestParam(value = "sort", defaultValue = "title") String sort,
-            @RequestParam(value = "direction", defaultValue = "asc") String direction) {
+            @RequestParam(value = "page", defaultValue = "0")
+            @ApiParam(value = "Page number of the results", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10")
+            @ApiParam(value = "size", defaultValue = "10") int size,
+            @RequestParam(value = "sort", defaultValue = "title")
+            @ApiParam(value = "Sort field", defaultValue = "title") String sort,
+            @RequestParam(value = "direction", defaultValue = "asc")
+            @ApiParam(value = "Sort direction", defaultValue = "asc") String direction) {
 
-        Sort sortable = Sort.by(sort);
-        if (direction.equalsIgnoreCase("desc")) {
-            sortable = sortable.descending();
-        }
 
-        PageRequest pageRequest = PageRequest.of(page, size, sortable);
+        PageRequest pageRequest = service.buildPageRequest(page, size, sort, direction);
 
         Page<NewsDtoResponse> newsPage = service.readAll(pageRequest);
 
@@ -49,47 +54,76 @@ public class NewsController implements BaseController<NewsDtoRequest, NewsDtoRes
 
     @Override
     @GetMapping(value = "/{id:\\d+}")
+    @ApiOperation(value = "Retrieve specific news with the supplied id", response = NewsDtoResponse.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved the news with the supplied id"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found"),
+            @ApiResponse(code = 500, message = "Application failed to process the request")
+    })
     public ResponseEntity<NewsDtoResponse> readById(@PathVariable Long id) {
-//        return ResponseEntity.ok(service.readById(id));
         return new ResponseEntity<>(service.readById(id), HttpStatus.OK);
     }
 
     @Override
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ResponseEntity<NewsDtoResponse> create(@Valid @RequestBody NewsDtoRequest createRequest) {
+    @ApiOperation(value = "Create a new news", response = NewsDtoResponse.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Successfully created a new news"),
+            @ApiResponse(code = 400, message = "Invalid news data"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
+    public ResponseEntity<NewsDtoResponse> create(
+            @Valid @RequestBody
+            @ApiParam(value = "News data", readOnly = true) NewsDtoRequest createRequest) {
         NewsDtoResponse newsDtoResponse = service.create(createRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(newsDtoResponse);
     }
 
     @Override
     @PutMapping("/{id}")
-    public ResponseEntity<NewsDtoResponse> update(@PathVariable Long id, @Valid @RequestBody NewsDtoRequest updateRequest) {
+    @ApiOperation(value = "Update an existing news", response = NewsDtoResponse.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully updated the news"),
+            @ApiResponse(code = 400, message = "Invalid news data"),
+            @ApiResponse(code = 404, message = "News not found"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
+    public ResponseEntity<NewsDtoResponse> update(
+            @PathVariable
+            @ApiParam(value = "News ID", readOnly = true) Long id,
+            @Valid @RequestBody
+            @ApiParam(value = "Updated news data", required = true) NewsDtoRequest updateRequest) {
         NewsDtoRequest updatedRequest = new NewsDtoRequest(id, updateRequest.title(), updateRequest.content(), updateRequest.authorId(), updateRequest.tagId());
         NewsDtoResponse newsDtoResponse = service.update(updatedRequest);
         return ResponseEntity.status(HttpStatus.OK).body(newsDtoResponse);
     }
 
-//    @Override
-//    @DeleteMapping(value = "/{id:\\d+}")
-//    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
-//        service.deleteById(id);
-//        return ResponseEntity.noContent().build();
-//    }
-
     @Override
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ApiOperation(value = "Delete a news article by ID")
+    @ApiResponses(value = {
+            @ApiResponse(code = 204, message = "Successfully deleted the news article"),
+            @ApiResponse(code = 404, message = "News not found"),
+            @ApiResponse(code = 500, message = "Internal server error")
+    })
     public void deleteById(@PathVariable Long id) {
         service.deleteById(id);
     }
 
     @GetMapping("/search")
-    public ResponseEntity<NewsDtoResponse> getNewsByParams(@Valid @RequestParam(required = false) String tagName,
-                                                           @Valid @RequestParam(required = false) Long tagId,
-                                                           @Valid @RequestParam(required = false) String authorName,
-                                                           @Valid @RequestParam(required = false) String title,
-                                                           @Valid @RequestParam(required = false) String content) {
+    @ApiOperation(value = "Get news articles by query parameters", response = NewsDtoResponse.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved news articles"),
+            @ApiResponse(code = 404, message = "No news articles found")
+    })
+    public ResponseEntity<NewsDtoResponse> getNewsByParams(
+            @Valid @RequestParam(required = false) @ApiParam(value = "Tag name") String tagName,
+            @Valid @RequestParam(required = false) @ApiParam(value = "Tag ID") Long tagId,
+            @Valid @RequestParam(required = false) @ApiParam(value = "Author name") String authorName,
+            @Valid @RequestParam(required = false) @ApiParam(value = "News title") String title,
+            @Valid @RequestParam(required = false) @ApiParam(value = "News content") String content) {
         NewsDtoResponse response = service.getNewsByParams(tagName, tagId, authorName, title, content);
         HttpStatus status = response != null ? HttpStatus.OK : HttpStatus.NOT_FOUND;
         return ResponseEntity.status(status).body(response);
